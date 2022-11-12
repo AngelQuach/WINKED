ac5b4996af01611da618abb785e7ff81eb521f52
yolanda <58274788+akaiyol@users.noreply.github.com>
2022-11-12 17:32:47 -0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
+import { AboutMe } from "./view/AboutMe"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <AboutMe />
+        <ColorModeSwitcher justifySelf="flex-end" />
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
